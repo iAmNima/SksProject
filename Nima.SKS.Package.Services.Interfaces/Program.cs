7cafb170c7f09bae250d29008f49a9dd30b4de98
iamnima <nima.bayat227@gmail.com>
2021-09-19 21:29:06 +0200

    first commit

---
+++ Nima.SKS.Package.Services.Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nima.SKS.Package.Services.Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
